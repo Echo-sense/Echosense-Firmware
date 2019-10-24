9bf923ad6c2a675fb87edb1842eabaf0e1cfaccc
drewcassidy <drewcassidy@me.com>
2019-10-23 17:32:49 -0700

    Add pins file

---
+++ src/pins.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2019 Team EchoSense 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ECHOSENSE_FIRMWARE_PINS_H
+#define ECHOSENSE_FIRMWARE_PINS_H
+
+// I2C pins
+#define I2C_SDA p0
+#define I2C_SCL p1
+
+// Power LED
+#define POWER_LED LED1
+
+#endif //ECHOSENSE_FIRMWARE_PINS_H
